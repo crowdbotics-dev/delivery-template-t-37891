7c895d1ed839a745940144ca97229d7fc7a06ac0
Crowdbotics <engineering@crowdbotics.com>
2022-10-10 19:05:39 +0000

    Update models

--- backend/delivery_template_t_37891/wsgi.py
+++ backend/delivery_template_t_37891/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'delivery_template_t_37891.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "delivery_template_t_37891.settings")
 
 application = get_wsgi_application()
